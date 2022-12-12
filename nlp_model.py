445941d66add986fb7a8b04c7c240c07b774c4f7
Nicola Orlando <nicolaorlandowork@gmail.com>
2022-12-12 14:57:40 +0100

    tmp model with crappy multiclass

--- nlp_model.py
+++ nlp_model.py
@@ -66,23 +66,29 @@ def make_bar_chart(x, y, title, plot_name):
     plt.savefig(plot_name)
 
 make_bar_chart(names_columns[2:6],average_labels[:4],'Average label values per research area','research_areas.png')
-make_bar_chart(names_columns[6:],average_labels[4:],'Average label values per research topic','topics.png')
+# These won't be used 
+# make_bar_chart(names_columns[6:],average_labels[4:],'Average label values per research topic','topics.png')
+
+train_df['target'] = train_df.apply(lambda x: 0 if x['Computer Science'] == 1 else 1  if x['Physics'] == 1 else 2  if x['Mathematics'] == 1 else 3, axis = 1)
+test_df['target'] = train_df.apply(lambda x: 0 if x['Computer Science'] == 1 else 1  if x['Physics'] == 1 else 2  if x['Mathematics'] == 1 else 3, axis = 1) 
+
+print(train_df.head())
 
 # From what we can see, there are no Nans in the dataset, but we have an unbalanced dataset.
 # The category 'Computer Science' has the largest average, of about 0.4, the lowest average is about 2 times lower (Mathematics). Overall we can't expect the classes to be balanced 
 
-# Train the model
-# Here I will train a model using the abstracts and the basic classification given by names_columns[2:6]
 
-# I want to check how well the model does in predicting the label 'Astrophysics of Galaxies' so I need to use part of the training dataset to be able to access the relevant topic label.
-# The topic labels are not avaiable in the given Train.csv file. 
-# X_train, X_test, y_train, y_test = train_test_split(train_df[['ABSTRACT','Computer Science', 'Physics', 'Mathematics', 'Statistics']],
-X_train, X_test, y_train, y_test = train_test_split(train_df[['ABSTRACT','Computer Science', 'Physics', 'Mathematics', 'Statistics']], 
-                                                    train_df['Astrophysics of Galaxies'], 
+# Train the model
+X_train, X_test, y_train, y_test = train_test_split(train_df['ABSTRACT'], 
+#                                                   train_df[['Computer Science', 'Physics', 'Mathematics', 'Statistics']],
+                                                    train_df['Computer Science'], 
                                                     random_state=0)
 
-print('inspecting types from train_test_split')
-print(type(X_train))
+X_train = train_df['ABSTRACT']
+y_train = train_df['target']
+
+X_test = test_df['ABSTRACT']
+y_test = test_df['target']
 
 # Here adding the other remaining features available at training and testing time which represent a basic classification of the abstracts (names_columns[2:6])
 def add_feature(X, feature_to_add):
@@ -96,43 +102,60 @@ def add_feature(X, feature_to_add):
 vectorizer = TfidfVectorizer()
 
 # Incrementally add the columns to the sparse feature matrix for both training and testing set. 
-X_vectorised = vectorizer.fit_transform(X_train['ABSTRACT'])
-X_vec_1 = add_feature(X_vectorised, X_train['Computer Science'])
-X_vec_2 = add_feature(X_vec_1, X_train['Mathematics'])
-X_vec_3 = add_feature(X_vec_2, X_train['Physics'])
-X_vec_fin = add_feature(X_vec_3, X_train['Statistics'])
+X_vectorised = vectorizer.fit_transform(X_train)
+#X_vec_1 = add_feature(X_vectorised, X_train['Computer Science'])
+#X_vec_2 = add_feature(X_vec_1, X_train['Mathematics'])
+#X_vec_3 = add_feature(X_vec_2, X_train['Physics'])
+#X_vec_fin = add_feature(X_vec_3, X_train['Statistics'])
 #
-X_test_vectorised = vectorizer.transform(X_test['ABSTRACT'])
-X_test_vec_1 = add_feature(X_test_vectorised, X_test['Computer Science'])
-X_test_vec_2 = add_feature(X_test_vec_1, X_test['Mathematics'])
-X_test_vec_3 = add_feature(X_test_vec_2, X_test['Physics'])
-X_test_vec_fin = add_feature(X_test_vec_3, X_test['Statistics'])
+X_test_vectorised = vectorizer.transform(X_test)
+#X_test_vec_1 = add_feature(X_test_vectorised, X_test['Computer Science'])
+#X_test_vec_2 = add_feature(X_test_vec_1, X_test['Mathematics'])
+#X_test_vec_3 = add_feature(X_test_vec_2, X_test['Physics'])
+#X_test_vec_fin = add_feature(X_test_vec_3, X_test['Statistics'])
 
 
 # Ameba 
 #cls = MultinomialNB(alpha=0.1)
 
 # Define a model with balanced classes and perform a basic HP scan. Can be made more complicated as needed. 
-cls = LogisticRegression(random_state=0, class_weight='balanced')
+cls = LogisticRegression(random_state=0, class_weight='balanced', multi_class='ovr')
+
 parameters = {'C': [0.5,1,1.5]}
 clf = GridSearchCV(cls, param_grid=parameters, scoring='roc_auc')  
-clf.fit(X_vec_fin, y_train)
-y_predict = clf.best_estimator_.predict(X_test_vec_fin)
+cls.fit(X_vectorised, y_train)
+# y_predict = clf.best_estimator_.predict(X_test_vectorised)
+#y_predict = cls.predict_proba(X_test_vectorised)
+y_predict = cls.predict(X_test_vectorised)
+
+y_predict_mod = np.array([1 if x == 0 else 0 for x in y_predict ])
+y_test_mod = np.array([1 if x == 0 else 0 for x in y_test ])
+
+print(type(y_predict))
+print(type(y_predict_mod))
+
+print(y_predict)
+print(y_predict_mod)
+
 
 # Evaluate a number of possible metrics 
-roc_auc = roc_auc_score(y_test, y_predict)
-recall = recall_score(y_test, y_predict)
-precision = precision_score(y_test, y_predict)
-accuracy = accuracy_score(y_test, y_predict)
+roc_auc = roc_auc_score(y_test_mod, y_predict_mod, multi_class='ovr')
+print(roc_auc)
+#recall = recall_score(y_test, y_predict)
+#precision = precision_score(y_test, y_predict)
+#accuracy = accuracy_score(y_test, y_predict)
+print('Printing prediction')
+print(y_predict_mod)
+print(y_test_mod)
 confusion_matrix = confusion_matrix(y_test,y_predict)
 
-print('Final metrics roc_auc = %f, recall = %f, precision = %f, accuracy =%f'%(roc_auc,recall,precision,accuracy))
+#print('Final metrics roc_auc = %f, recall = %f, precision = %f, accuracy =%f'%(roc_auc,recall,precision,accuracy))
 print('Confusion matrix')
 print(confusion_matrix)
 
-import seaborn as sn
-fig = plt.figure(figsize = (4, 4))
-plt.title('Confusion matrix')
-ax = sn.heatmap(confusion_matrix, cmap='Oranges', annot=True)
-plt.tight_layout()
-plt.savefig('heatmap.png')
+#import seaborn as sn
+#fig = plt.figure(figsize = (4, 4))
+#plt.title('Confusion matrix')
+#ax = sn.heatmap(confusion_matrix, cmap='Oranges', annot=True)
+#plt.tight_layout()
+#plt.savefig('heatmap.png')
